f552f19ed53a5439ced3c71bd3186697d0e5c2ac
Tejdeep2003 <tejdeepgutta03@gmail.com>
2023-10-31 10:41:49 +0530

    added functions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.Scanner;
+import java.lang.Math;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -22,25 +24,36 @@ public class Main {
             System.out.println("2. Subtraction");
             System.out.println("3. Multiplication");
             System.out.println("4. Division");
-            System.out.println("5. Exit");
+            System.out.println("5. Power");
+            System.out.println("6. log");
+            System.out.println("7. square root");
+            System.out.println("8. exit");
+
 
             int choice = myObj.nextInt();
-
-            if(choice==5)
+            int x, y=0;
+            if(choice==8)
             {
                 logger.info("End of Execution");
                 System.out.println("Thank you for using Calculator!!");
                 break;
             }
+            else if(choice==6 || choice==7)
+            {
+                System.out.println("Enter first operand:");
 
-            System.out.println("Enter first operand:");
+                x = myObj.nextInt();
+            }
+            else
+            {
+                System.out.println("Enter first operand:");
 
-            int x = myObj.nextInt();
+                x = myObj.nextInt();
 
-            System.out.println("Enter second operand:");
+                System.out.println("Enter second operand:");
 
-            int y = myObj.nextInt();
-
+                y = myObj.nextInt();
+            }
             System.out.println("Result: ");
             switch(choice)
             {
@@ -55,6 +68,15 @@ public class Main {
                     break;
                 case 4:
                     System.out.println(div(x,y));
+                    break;
+                case 5:
+                    System.out.println(Pow(x,y));
+                    break;
+                case 6:
+                    System.out.println(Log(x));
+                    break;
+                case 7:
+                    System.out.println(Sqrt(x));
                     break;
             }
 
@@ -94,5 +116,28 @@ public class Main {
             return z;
         }
         return 0;
+    }
+
+    public static int Pow(int x, int y)
+    {
+        logger.info("START OP: Pow");
+        int z = (int) Math.pow(x,y);
+        logger.info("END OP: Pow");
+        return z;
+    }
+    public static int Log(int x)
+    {
+        logger.info("START OP: Log");
+        int z = (int)(Math.log(x) / Math.log(2));
+        logger.info("END OP: Log");
+        return z;
+    }
+
+    public static int Sqrt(int x)
+    {
+        logger.info("START OP: Sqrt");
+        int z = (int)Math.sqrt(x) ;
+        logger.info("END OP: Sqrt");
+        return z;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -35,7 +35,8 @@ public class CalculatorTest {
         int expectedResult = 0;
         Assert.assertNotEquals(expectedResult, Main.sub(a,
                 b));
-    }@Test
+    }
+    @Test
     public void test_mul_positive(){
         int a = 1;
         int b = 2;
@@ -50,7 +51,8 @@ public class CalculatorTest {
         int expectedResult = 0;
         Assert.assertNotEquals(expectedResult, Main.mul(a,
                 b));
-    }@Test
+    }
+    @Test
     public void test_div_positive(){
         int a = 6;
         int b = 2;
@@ -65,5 +67,48 @@ public class CalculatorTest {
         int expectedResult = 0;
         Assert.assertNotEquals(expectedResult, Main.div(a,
                 b));
+    }
+
+    @Test
+    public void test_pow_positive(){
+        int a = 2;
+        int b = 2;
+        int expectedResult = 4;
+        Assert.assertEquals(expectedResult, Main.Pow(a,
+                b));
+    }
+    @Test
+    public void test_pow_negative(){
+        int a = 6;
+        int b = 2;
+        int expectedResult = 20;
+        Assert.assertNotEquals(expectedResult, Main.Pow(a,
+                b));
+    }
+
+    @Test
+    public void test_Log_positive(){
+        int a = 1024;
+        int expectedResult = 10;
+        Assert.assertEquals(expectedResult, Main.Log(a));
+    }
+    @Test
+    public void test_Log_negative(){
+        int a = 4;
+        int expectedResult = 20;
+        Assert.assertNotEquals(expectedResult, Main.Log(a));
+    }
+
+    @Test
+    public void test_Sqrt_positive(){
+        int a = 4;
+        int expectedResult = 2;
+        Assert.assertEquals(expectedResult, Main.Log(a));
+    }
+    @Test
+    public void test_Sqrt_negative(){
+        int a = 4;
+        int expectedResult = 20;
+        Assert.assertNotEquals(expectedResult, Main.Log(a));
     }
 }
